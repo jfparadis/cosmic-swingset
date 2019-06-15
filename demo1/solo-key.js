5bac594fbec61c687bdccc95f6b60d44d8d6e57b
Brian Warner <warner@lothar.com>
2019-06-15 12:04:53 +0200

    demo1/solo-key.js: add placeholder for the solo-node key

---
+++ demo1/solo-key.js
@@ -0,0 +1,5 @@
+// This file is used by bootstrap.js to figure out the solo node's pubkey.
+// The file is initially empty, and must be filled in before the chain can be
+// started.
+
+export const soloKey = undefined;
